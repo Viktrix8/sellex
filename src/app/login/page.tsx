bbb9385a30f46123100cfff2474a816acf709698
Viktrix <najetak.viktrixeu@gmail.com>
2025-03-05 13:57:12 +0100

    change login page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,25 @@
 import { LoginForm } from "@/components/login-form";
+import Image from "next/image";
 import React from "react";
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative bg-[#2F2F2F] hidden brightness-90 lg:flex items-center justify-center">
+        <Image
+          src="/logo.png"
+          alt="Image"
+          height={400}
+          width={400}
+          className="object-cover"
+        />
       </div>
     </div>
   );
