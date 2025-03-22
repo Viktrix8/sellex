4c7f231e986bdb6c30a52c3d2441a056580f654c
Viktor Sopko <najetak.viktrixeu@gmail.com>
2025-03-22 20:18:31 +0100

    add google adsense.

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/navbar";
 import FooterSection from "@/components/sections/footer/default";
+import Head from "next/head";
+import Script from "next/script";
 import React from "react";
 
 type Props = {
@@ -9,6 +11,13 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <>
+      <Head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8435095568140595"
+          crossOrigin="anonymous"
+        ></Script>
+      </Head>
       <Navbar />
       <div className="container mx-auto p-4">{children}</div>
       <FooterSection />
